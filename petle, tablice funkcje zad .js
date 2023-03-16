dfbfba7346d63e8ef457cfc6b68f5dde253c6265
KrzysztofKadziolka11 <96467048+KrzysztofKadziolka11@users.noreply.github.com>
2023-03-16 08:42:41 +0100

    Create petle, tablice funkcje zad .js

---
+++ petle, tablice funkcje zad .js
@@ -0,0 +1,93 @@
+// 1. Napisz program w JavaScript, który akceptuje dwie liczby całkowite i wyświetla większą. (zastosuj prompt() w celu wprowadzenia liczb do programu).
+
+
+
+// 2. Napisz program w JavaScript, który będzie iterować od 0 do 15. Dla każdej iteracji niech sprawdza czy liczba jest parzysta, czy nieparzysta, oraz wyświetla komunikat. 
+
+// Przykładowa odpowiedz: 
+// 1 jest liczbą nieparzystą
+// 2 jest liczbą parzystą
+// .
+// .
+// .
+// 15 jest liczbą nieparzystą
+
+
+
+// 3. Napisz program w JavaScript, który iteruje liczby całkowite od 1 do 100. Dla każdej liczby która jest wielokrotnością liczby 3, niech wypisze obok liczby, słowo "Fizz". Dla liczby która jest wielokrotnością liczby 5, wypisze słowo: "Buzz". Jeżeli liczba jest wielokrtnością liczby 3 i 5, należy wyświetlić słowo "FizzBuzz". Dla każdej innej liczby, należy wyświetlić bieżącą liczbę.
+
+//Przykładowa odpowiedź: 
+// 2
+// 3 - Fizz
+// 5 - Buzz
+// 15 - FizzBuzz
+
+// 4. Napisz program, który zsumuje liczby będące wielokrotnościami liczb 3 lub 5, wśród wszystkich liczb całkowitych poniżej 1000.
+
+// Wynik: 233168
+
+
+// 1. Napisz program sprawdzający, czy podana wartość jest tablicą. 
+
+
+// 2. Napisz program zwracający pierwszy element tablicy. Program powinien umożliwiać wprowadzenie zmiennej, dzięki której zostanie zwrócone "n", elementów tablicy.
+
+
+// Dodatkowe informacje:
+// Jeśli tablica jest pusta -> zwróć komunikat
+// Jeśli tablica nie jest pusta, a użytkownik nie podał żadnej wartości "n" -> zwróć pierwszy element tablicy.
+// Jeśli tablica nie jest pusta, i użytkownik podał wartość "n" -> zwróć "n" elementów tablicy, zaczynając od pozycji 0.
+
+// 3. Napisz program w JavaScript, który wypisze elementy poniższej tablicy w formacie podanym poniżej.
+
+
+// Przykładowa odpowiedź:
+/*
+row 0
+ 1
+ 2
+ 1
+ 24
+row 1
+ 8
+ 11
+ 9
+ 4
+*/
+
+// 4. Napisz program w JavaScript, który znajdzie sumę kwadratów wektora liczbowego.
+
+// Przykładowa odpowiedź:
+// Dla liczb: 0,1,2,3,4 -> wynik: 30
+
+// 5. Napisz program w JS, czyszczący tablicę z pustych wartości takich jak: 'null', '0', "", 'false', 'undefined', 'NaN'.
+
+// Przykładowa odpowiedź:
+// dla tablicy: [NaN, 0, 15, false, -22, '',undefined, 47, null] -> wynik: [15, -22, 47]
+
+// 6. (Dodatkowe). Napisz program w JavaScript, który przyjmuje liczbę jako dane wejściowe i wstawia kreski (-) między każdą z dwóch liczb parzystych.
+
+// Przykładowa odpowiedź:
+// dla liczby: 025468 -> odpowiedź: 0-254-6-8
+
+
+
+
+
+// 1. Napisz funkcję JavaScript, która przyjmuje liczbę jako parametr i sprawdza, czy liczba jest liczbą pierwszą, czy nie
+
+// Przykładowa odpowiedź:
+// console.log(check_prime(37)) -> true;
+
+// 2. Napisz funkcję JavaScript, która zwraca elementy tablicy większe lub równe niż podana przez użytkownika liczba.
+
+// Przykładowa odpowiedź: 
+// dla tablicy [11, 45, 4, 31, 64, 10] i liczby 45, zwróci -> [45,64]
+
+
+
+
+
+
+
+
